2c1f89d04b4ea7e3375566a359a7735fab441515
qtuancr261 <tuancr261@gmail.com>
2018-01-11 17:18:24 +0700

    Test client send and recv function

--- Socket/SocketClient/main.cpp
+++ Socket/SocketClient/main.cpp
@@ -2,6 +2,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <cstring>
 #include <iostream>
 using namespace std;
 int main(int argc, char *argv[])
@@ -19,6 +20,12 @@ int main(int argc, char *argv[])
     if (connect(client_socket, (sockaddr*)&server, sizeof(server)) < 0)
         cerr << "Connection Failed";
     else
-        cout << "Connection Successfully" << endl;
+        cout << "Connection Successfully " << endl;
+    char messageFromServer[2000]{};
+    if (recv(client_socket, messageFromServer, 2000, 0) > 0)
+        cout << messageFromServer;
+    char messageFromClient[2000]{};
+    cin.getline(messageFromClient, 2000);
+    send(client_socket, messageFromClient, strlen(messageFromClient), 0);
     return 0;
 }
