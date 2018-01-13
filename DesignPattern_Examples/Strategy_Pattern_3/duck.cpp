7392a38c0ee85482a140f90771efe79037497030
qtuancr261 <tuancr261@gmail.com>
2018-01-13 22:09:59 +0700

    A base class describle duck species

--- DesignPattern_Examples/Strategy_Pattern_3/duck.cpp
+++ DesignPattern_Examples/Strategy_Pattern_3/duck.cpp
@@ -1,6 +1,36 @@
 #include "duck.h"
 
-Duck::Duck()
+Duck::Duck(FlyBehavior *flyBehaviorPtr, QuackBehavior *quackBehaviorPtr)
+    : flyBehavior{flyBehaviorPtr},
+      quackBehavior{quackBehaviorPtr}
 {
+    // default constructor
+}
 
+void Duck::setFlyBehavior(FlyBehavior * const flyPtr)
+{
+    delete flyBehavior;
+    flyBehavior = flyPtr;
+}
+
+std::string Duck::doFly() const
+{
+    return flyBehavior->fly();
+}
+
+void Duck::setQuackBehavior(QuackBehavior * const quackPtr)
+{
+    delete quackBehavior;
+    quackBehavior = quackPtr;
+}
+
+std::string Duck::doQuack() const
+{
+    return quackBehavior->quack();
+}
+
+Duck::~Duck()
+{
+    delete flyBehavior;
+    delete quackBehavior;
 }
--- DesignPattern_Examples/Strategy_Pattern_3/duck.h
+++ DesignPattern_Examples/Strategy_Pattern_3/duck.h
@@ -1,11 +1,19 @@
 #ifndef DUCK_H
 #define DUCK_H
-
-
+#include "behavior/flybehavior.h"
+#include "behavior/quackbehavior.h"
 class Duck
 {
+private:
+    FlyBehavior* flyBehavior;
+    QuackBehavior* quackBehavior;
 public:
-    Duck();
+    Duck(FlyBehavior* flyBehaviorPtr = nullptr, QuackBehavior* quackBehaviorPtr = nullptr);
+    void setFlyBehavior(FlyBehavior * const flyPtr);
+    string doFly() const;
+    void setQuackBehavior(QuackBehavior * const quackPtr);
+    string doQuack() const;
+    virtual ~Duck();
 };
 
 #endif // DUCK_H
