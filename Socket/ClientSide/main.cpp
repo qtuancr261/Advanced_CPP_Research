bb50f9496dddb22cebe28c81c6a8d107bdf3a248
qtuancr261 <tuancr261@gmail.com>
2018-01-18 15:18:17 +0700

    create a thread to handle recv stream

--- Socket/ClientSide/main.cpp
+++ Socket/ClientSide/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
     Client_Core client;
     client.exec();
     std::thread myThreadS(&Client_Core::handleSendStream, &client);
+    std::thread myThreadR(&Client_Core::handleReceiveStream, &client);
     myThreadS.join();
+    myThreadR.join();
     return 0;
 }
