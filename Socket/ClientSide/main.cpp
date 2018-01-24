355ed483aa75bd8b3c354fe55a45dae89d9c5212
qtuancr261 <tuancr261@gmail.com>
2018-01-24 15:54:25 +0700

    using QString(const char* arg) instead of std::string(const char* arg) :3

--- Socket/ClientSide/main.cpp
+++ Socket/ClientSide/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     Client_Core client;
     for (int i{}; i < argc; i++)
         std::printf(argv[i]);
-    client.exec(string(argv[1]));
+    client.exec(QString(argv[1]));
     std::thread myThreadS(&Client_Core::handleSendStream, &client);
     std::thread myThreadR(&Client_Core::handleReceiveStream, &client);
     myThreadS.join();
