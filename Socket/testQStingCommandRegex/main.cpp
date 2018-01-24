2494676df9a16ecc01cf3521d73f37fb6ff07171
qtuancr261 <tuancr261@gmail.com>
2018-01-24 15:58:54 +0700

    test QString and char*

--- Socket/testQStingCommandRegex/main.cpp
+++ Socket/testQStingCommandRegex/main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
-
+#include <QString>
+#include <iostream>
+#include <QtDebug>
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    char cString[]{"tuan send: <FILE> testFile.odt 125"};
+    QString cmd(cString);
+    for (QString subCmd : cmd.split(" "))
+        qDebug() << subCmd;
+    qDebug() << cmd.section(" ", -1, -1);
+    if (cmd.section(" ", 2, 2) == QString("<FILE>"))
+        qDebug() << "perfect";
+    char cString2[]{"\n"};
+    std::printf("-- %s ", cString2);
 
-    return a.exec();
 }
