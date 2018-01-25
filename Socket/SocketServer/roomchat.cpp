423c230ee11e07a9277300a9a8a70e01dead4273
qtuancr261 <tuancr261@gmail.com>
2018-01-25 10:24:45 +0700

    A class for managing several clients that connecto to our server socket

--- Socket/SocketServer/roomchat.cpp
+++ Socket/SocketServer/roomchat.cpp
@@ -1,6 +1,27 @@
 #include "roomchat.h"
 
-RoomChat::RoomChat()
+QString RoomChat::getName() const
 {
+    return name;
+}
 
+void RoomChat::setName(const QString &value)
+{
+    name = value;
+}
+
+vector<clientPtr> &RoomChat::getClientsInRoom()
+{
+    return clientsInRoom;
+}
+
+void RoomChat::addAClientToRoom(clientPtr client)
+{
+    clientsInRoom.push_back(move(client));
+}
+
+RoomChat::RoomChat(QString defName)
+    : name{defName}
+{
+    // default constructor
 }
--- Socket/SocketServer/roomchat.h
+++ Socket/SocketServer/roomchat.h
@@ -1,11 +1,26 @@
 #ifndef ROOMCHAT_H
 #define ROOMCHAT_H
-
-
+#include "client.h"
+#include <memory>
+#include <vector>
+#include <utility>
+using std::vector;
+using std::unique_ptr;
+using std::make_unique;
+using clientPtr = unique_ptr<client>;
+using std::move;
 class RoomChat
 {
+private:
+    QString name;
+    vector<clientPtr> clientsInRoom;
 public:
-    RoomChat();
+    RoomChat(QString defName);
+
+    QString getName() const;
+    void setName(const QString &value);
+    vector<clientPtr>& getClientsInRoom();
+    void addAClientToRoom(clientPtr client);
 };
 
 #endif // ROOMCHAT_H
