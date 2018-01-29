bbbb15fb816ba36eab6c9ba23491256b67634324
qtuancr261 <tuancr261@gmail.com>
2018-01-29 10:07:08 +0700

    add a weak_ptr for reserve a ptr to the room where current client stayed

--- Socket/SocketServer/client.h
+++ Socket/SocketServer/client.h
@@ -7,19 +7,24 @@
 #include "roomchat.h"
 using std::list;
 using std::string;
-
+using std::weak_ptr;
+using std::shared_ptr;
+using std::make_shared;
 class RoomChat;
 class client
 {
 private:
     int id;
     QString name;
+    weak_ptr<RoomChat> currentStayedRoom;
 public:
     client(int id, QString name = "noname");
     int getId() const;
     void setId(int value);
     QString getName() const;
     void setName(const QString &value);
+    weak_ptr<RoomChat>& getCurrentStayedRoom();
+    void setCurrentStayedRoom(const shared_ptr<RoomChat> &value);
 };
 
 #endif // CLIENT_H
