2e0d48c729a7dca8efd22af890a9c15357b8a9ce
qtuancr261 <tuancr261@gmail.com>
2018-01-30 14:33:00 +0700

    I'll leave TMA temporarily

--- Socket/testQStingCommandRegex/main.cpp
+++ Socket/testQStingCommandRegex/main.cpp
@@ -2,6 +2,10 @@
 #include <QString>
 #include <iostream>
 #include <QtDebug>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+using namespace std;
 int main(int argc, char *argv[])
 {
     char cString[]{"tuan send: <FILE> testFile.odt 125"};
@@ -13,5 +17,8 @@ int main(int argc, char *argv[])
         qDebug() << "perfect";
     char cString2[]{"\n"};
     std::printf("-- %s ", cString2);
+    this_thread::sleep_for(chrono::microseconds(50000));
+    std::printf("-- usleep");
+    return 0;
 
 }
