5f7b03b920fdc54f8f779d2d0c61bb389d392d7a
qtuancr261 <tuancr261@gmail.com>
2018-02-14 16:43:22 +0700

    Subject Interface

--- DesignPattern_Examples/Observer_Pattern/WheatherStation/subject/subject.h
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/subject/subject.h
@@ -1,4 +1,10 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
-
+#include "../observer/observer.h"
+class Subject
+{
+    virtual void registerObserver(const Observer& newObserver) = 0;
+    virtual void removeObserver(const Observer& anObserver) = 0;
+    virtual void notifyAllObserver() const = 0;
+};
 #endif // SUBJECT_H
