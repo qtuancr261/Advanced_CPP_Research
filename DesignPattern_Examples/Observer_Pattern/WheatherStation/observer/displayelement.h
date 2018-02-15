0a69e5d9c07ceb1a86e058c7ea710562437748a7
qtuancr261 <tuancr261@gmail.com>
2018-02-15 15:43:11 +0700

    1 day remaning :3

--- DesignPattern_Examples/Observer_Pattern/WheatherStation/observer/displayelement.h
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/observer/displayelement.h
@@ -2,6 +2,6 @@
 #define DISPLAYELEMENT_H
 class DisplayElement
 {
-    virtual void display() const = 0;
+    virtual void display(double temperature, double humidity, double pressure) const = 0;
 };
 #endif // DISPLAYELEMENT_H
