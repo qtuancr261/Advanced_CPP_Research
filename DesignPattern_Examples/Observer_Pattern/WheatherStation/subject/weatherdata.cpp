280c11171a6de5a28b1a9d32b4569de656e19b63
qtuancr261 <tuancr261@gmail.com>
2018-02-19 21:21:05 +0700

    Implement WeatherData as a concrete Subject

--- DesignPattern_Examples/Observer_Pattern/WheatherStation/subject/weatherdata.cpp
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/subject/weatherdata.cpp
@@ -4,3 +4,21 @@ WeatherData::WeatherData()
 {
 
 }
+
+void WeatherData::registerObserver(const ObserverPtr& newObserver)
+{
+    observers.push_back(newObserver);
+}
+
+void WeatherData::removeObserver(const ObserverPtr& anObserver)
+{
+    observers.remove(anObserver);
+}
+
+void WeatherData::notifyAllObserver() const
+{
+    for (auto& observer : observers)
+    {
+        observer->update(temperature, humidity, pressure);
+    }
+}
--- DesignPattern_Examples/Observer_Pattern/WheatherStation/subject/weatherdata.h
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/subject/weatherdata.h
@@ -1,11 +1,22 @@
 #ifndef WEATHERDATA_H
 #define WEATHERDATA_H
-
-
-class WeatherData
+#include "subject.h"
+#include "list"
+#include "vector"
+using std::list;
+using std::vector;
+class WeatherData : public Subject
 {
+private:
+    list<ObserverPtr> observers;
+    double temperature;
+    double pressure;
+    double humidity;
 public:
     WeatherData();
+    virtual void registerObserver(const ObserverPtr& newObserver) override;
+    virtual void removeObserver(const ObserverPtr& anObserver) override;
+    virtual void notifyAllObserver() const override;
 };
 
 #endif // WEATHERDATA_H
