d9e7c4257893792e41615445b00fb769e6b8ea1d
qtuancr261 <tuancr261@gmail.com>
2018-02-22 16:19:10 +0700

    test with 3 displays and 1 weatherData

--- DesignPattern_Examples/Observer_Pattern/WheatherStation/main.cpp
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/main.cpp
@@ -1,8 +1,18 @@
 #include <QCoreApplication>
-
+#include "subject/weatherdata.h"
+#include "observer/currentconditionsdisplay.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    shared_ptr<WeatherData> weatherData{make_shared<WeatherData>()};
+    shared_ptr<CurrentConditionsDisplay> display1{make_shared<CurrentConditionsDisplay>()};
+    shared_ptr<CurrentConditionsDisplay> display2{make_shared<CurrentConditionsDisplay>()};
+    shared_ptr<CurrentConditionsDisplay> display3{make_shared<CurrentConditionsDisplay>()};
+    display1->setSubject(weatherData);
+    display2->setSubject(weatherData);
+    weatherData->setMeasurements(27.0, 100, 68);
+    weatherData->removeObserver(display2);
+    weatherData->setMeasurements(28.3, 100, 65);
+    return 0;
     return a.exec();
 }
