c81a2b30e259cf0effcefdd5b113f6ac0d24d240
qtuancr261 <tuancr261@gmail.com>
2018-02-22 16:11:10 +0700

    Create a concrete Display inheriting from Observer, DisplayElement and  enable_shared_from_this<>

--- DesignPattern_Examples/Observer_Pattern/WheatherStation/observer/currentconditionsdisplay.h
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/observer/currentconditionsdisplay.h
@@ -1,11 +1,28 @@
 #ifndef CURRENTCONDITIONSDISPLAY_H
 #define CURRENTCONDITIONSDISPLAY_H
-
-
-class CurrentConditionsDisplay
+#include "observer.h"
+#include "displayelement.h"
+#include "../subject/subject.h"
+using std::weak_ptr;
+using std::enable_shared_from_this;
+class CurrentConditionsDisplay : public Observer, public DisplayElement, public enable_shared_from_this<CurrentConditionsDisplay>
 {
+private:
+    double temperature;
+    double humidity;
+    double pressure;
+    weak_ptr<Subject> weatherData;
 public:
     CurrentConditionsDisplay();
+    void setSubject(const shared_ptr<Subject>& weatherData);
+    // DisplayElement interface
+public:
+    virtual void display() const override;
+
+    // Observer interface
+public:
+    virtual void update(double temperature, double humidity, double pressure) override;
+    virtual ~CurrentConditionsDisplay();
 };
 
 #endif // CURRENTCONDITIONSDISPLAY_H
