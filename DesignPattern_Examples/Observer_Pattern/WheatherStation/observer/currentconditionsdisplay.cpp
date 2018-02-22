77c4ff28525cb9f52a4444bf8b85140837c262a0
qtuancr261 <tuancr261@gmail.com>
2018-02-22 16:13:26 +0700

    use a weak_ptr to hold the Subject set from setSubject() function | Be carefull with share_from_this() function :3

--- DesignPattern_Examples/Observer_Pattern/WheatherStation/observer/currentconditionsdisplay.cpp
+++ DesignPattern_Examples/Observer_Pattern/WheatherStation/observer/currentconditionsdisplay.cpp
@@ -1,6 +1,35 @@
 #include "currentconditionsdisplay.h"
 
 CurrentConditionsDisplay::CurrentConditionsDisplay()
+    : temperature{}, humidity{}, pressure{}, weatherData{}
 {
 
+}
+
+void CurrentConditionsDisplay::setSubject(const shared_ptr<Subject> &weatherData)
+{
+    weatherData->registerObserver(shared_from_this());
+    this->weatherData = weatherData;
+}
+
+
+void CurrentConditionsDisplay::display() const
+{
+    cout << "Current Conditions : ";
+    cout << setw(10) << "Temp: " << setw(4) << temperature << " C Degrees" <<endl;
+    cout << setw(10) << "Humidity: " << setw(4) << humidity << " %" <<endl;
+    cout << setw(10) << "Pressure: " << setw(4) << pressure << endl;
+}
+
+void CurrentConditionsDisplay::update(double temperature, double humidity, double pressure)
+{
+    this->temperature = temperature;
+    this->humidity = humidity;
+    this->pressure = pressure;
+    display();
+}
+
+CurrentConditionsDisplay::~CurrentConditionsDisplay()
+{
+    qDebug() << "Display has been deleted";
 }
