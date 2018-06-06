46dcb1d88f07646f4f0d9d79f3ea5b1652d049fd
qtuancr261 <tuancr261@gmail.com>
2018-06-06 12:09:29 +0700

    HybridHardDrive class is both a HardDiskDrive and a SolidStateDrive

--- DiamonProblem/hybridharddrive.h
+++ DiamonProblem/hybridharddrive.h
@@ -1,8 +1,9 @@
 #ifndef HYBRIDHARDDRIVE_H
 #define HYBRIDHARDDRIVE_H
+#include "harddiskdrive.h"
+#include "solidstatedrive.h"
 
-
-class HybridHardDrive
+class HybridHardDrive : public HardDiskDrive, public SolidStateDrive
 {
 public:
     HybridHardDrive();
