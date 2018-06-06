9e8caf4882ed9a84c6b935dafb57bc252e307bd4
qtuancr261 <tuancr261@gmail.com>
2018-06-06 12:12:59 +0700

    An example that illustrate the Diamon Problem when using Multiple Inheritance in C++

--- DiamonProblem/main.cpp
+++ DiamonProblem/main.cpp
@@ -1,8 +1,15 @@
 #include <QCoreApplication>
-
+#include <iostream>
+#include "hybridharddrive.h"
+using std::cout;
+using std::endl;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    HybridHardDrive storage{};
+    // getId is belongs to both HardDiskDrive and SolidStateDrive
+    // The compiler will get an ambiguos error if you not use "vitual" inheritance
+    cout << storage.getId();
+    return 0;
     return a.exec();
 }
