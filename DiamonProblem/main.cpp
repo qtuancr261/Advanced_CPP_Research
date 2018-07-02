d821a63063974f059b41bf929012dbc71b516cb2
qtuancr261 <tuancr261@gmail.com>
2018-07-02 19:38:03 +0700

    Remove Qt Headers

--- DiamonProblem/main.cpp
+++ DiamonProblem/main.cpp
@@ -1,11 +1,9 @@
-#include <QCoreApplication>
 #include <iostream>
 #include "hybridharddrive.h"
 using std::cout;
 using std::endl;
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
     HybridHardDrive storage{};
     // getId is belongs to both HardDiskDrive and SolidStateDrive
     // The compiler will get an ambiguos error if you not use "vitual" inheritance
@@ -13,5 +11,4 @@ int main(int argc, char *argv[])
 
     //SolidStateDrive ssd{};
     return 0;
-    return a.exec();
 }
