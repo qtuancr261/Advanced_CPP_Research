7894079a75c56f282c8dd051352dad9794cf952a
qtuancr261 <tuancr261@gmail.com>
2019-05-10 07:26:44 +0700

    Updated build config

--- MultiThread_Examples/ConcurrentWorld/main.cpp
+++ MultiThread_Examples/ConcurrentWorld/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <thread>
 using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
+int say() {
+    cout << "Hello concurrent world " << endl;
+    cout << " ------" << endl;
+    return 1;
+}
+int main() {
+    thread worker(say);
+    worker.join();
     return 0;
 }
