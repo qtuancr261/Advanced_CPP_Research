cd96cea9cea5243f70dbfcb5426ae1d837f3166c
qtuancr261 <tuancr261@gmail.com>
2019-05-15 08:05:42 +0700

    Updated argument

--- ProcessCpuUsage/main.cpp
+++ ProcessCpuUsage/main.cpp
@@ -1,13 +1,25 @@
-#include <iostream>
+#include "CPUClock.h"
 #include "processutil.h"
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        // cout << "./Bin processID traceTimeInSec";
+        return 0;
+    }
     int pid = atoi(argv[1]);
-//    while (true) {
-//       //std::cout << ProcessUtil::GetCpuUsage()
-//    }
-    std::cout << " Process " << pid << " " <<  ProcessUtil::GetCpuUsage(pid) <<std::endl;
+    int durationInSec = atoi(argv[2]);
+
+    CpuClock clock;
+    int count = 0;
+    double total = 0;
+    while (clock.elapsedSec() <= durationInSec) {
+        double currentCPUUsage = ProcessUtil::GetCpuUsage(pid);
+        if (count > 0) total += currentCPUUsage;
+        // cout << " Process " << pid << " " << currentCPUUsage << endl;
+        count++;
+        sleep(1);
+    }
+    // cout << "Average : " << total / durationInSec << " %" << endl;
     return 0;
 }
