829ddce5acdc84f5d330ade7ba685904b0b36a2a
qtuancr261 <tuancr261@gmail.com>
2019-08-04 19:47:50 +0700

    Added establishBasicClient

--- Socket/BasicSocket/main.cpp
+++ Socket/BasicSocket/main.cpp
@@ -13,10 +13,10 @@ int createTCPSocketV6() { return socket(AF_INET6, SOCK_STREAM, 0); }
 int createUDPSocketV4() { return socket(AF_INET, SOCK_DGRAM, 0); }
 int createUDPSocketV6() { return socket(AF_INET6, SOCK_DGRAM, 0); }
 
-void establishBasicServer(uint16_t port) {
+[[noreturn]] void establishBasicServer(uint16_t port) {
     int sock_server{createTCPSocketV4()};
     if (sock_server < 0) {
-        printf("Couldn't create socket \n");
+        cerr << "Couldn't create socket";
         exit(1);
     }
 
@@ -27,12 +27,12 @@ void establishBasicServer(uint16_t port) {
     serverInfo.sin_family = AF_INET;
     // bind the created socket to an address + port
     if (bind(sock_server, (sockaddr*)(&serverInfo), sizeof(serverInfo)) < 0) {
-        printf("Couldn't bind server info to socket %d", sock_server);
+        cerr << "Couldn't bind server info to socket " << sock_server;
         exit(1);
     }
 
     if (listen(sock_server, 5) < 0) {
-        printf("Couldn't listen to prepare incoming connection ");
+        cerr << "Couldn't listen to prepare incoming connection ";
         exit(1);
     }
 
@@ -42,7 +42,7 @@ void establishBasicServer(uint16_t port) {
     int clientLen{sizeof(sockaddr_in)};
     int sock_client{};
     while (true) {
-        sock_client = accept(sock_server, (sockaddr*)(&clientInfo), (socklen_t*)(&clientLen));
+        sock_client = accept(sock_server, (sockaddr*)(&clientInfo), (socklen_t*)&clientLen);
         if (sock_client <= 0) {
             continue;
         }
@@ -50,59 +50,24 @@ void establishBasicServer(uint16_t port) {
         write(sock_client, welcomeMsg.c_str(), welcomeMsg.size());
     }
 }
+
+[[noreturn]] void establishBasicClient(const char* host, uint16_t port) {
+    int sock_client{createTCPSocketV4()};
+    if (sock_client < 0) {
+        cerr << "Couldn't create socket \n";
+        exit(1);
+    }
+
+    sockaddr_in serverInfo{};
+    serverInfo.sin_addr.s_addr = inet_addr(host);
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_port = htons(port);
+}
 int main(int argc, char* argv[]) {
     for (int i = 0; i < argc; i++) {
         cout << argv[i] << endl;
     }
 
-    if (!establishBasicServer(2610)) {
-        cerr << "Failed to establish server";
-    } else {
-        cout << "Establised server successfully ";
-    }
-    //        ;
-    //    // Firstly, we must create a socket
-    //    int socket_desc;
-    //    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    //    // AF_INET stands for Address family IP ver 4
-    //    // SOCK_STREAM means TCP protocol
-    //    if (socket_desc == -1) {
-    //        cout << "Cannot create socket" << endl;
-    //        exit(1);
-    //    }
-
-    //    // Indicate a server with ip address and port number to connect
-    //    sockaddr_in server;
-    //    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    //    // server.sin_addr.s_addr = INADDR_ANY;
-    //    server.sin_family = AF_INET;
-    //    server.sin_port = htons(11007);
-
-    //    if (bind(socket_desc, (sockaddr*)(&server), sizeof(server)) < 0) {
-    //        cerr << "bind failed";
-    //        exit(1);
-    //    }
-    // connect to a server
-    //    if (connect(socket_desc, (sockaddr*)&server, sizeof(server)) < 0)
-    //        cout << "Error" << endl;
-    //    else
-    //        cout << "Connected" << endl;
-
-    //    // Send data over socket(message)
-    //    char* message{"Welcome to POSIX socket lib"};
-    //    if (send(socket_desc, message, strlen(message), 0) < 0)
-    //        cout << "send function failed" << endl;
-    //    else
-    //        cout << "Data transfering successfully" << endl;
-
-    //    // Receive data on socket
-    //    char receivedMessage[2000]{};
-    //    if (recv(socket_desc, receivedMessage, 2000, 0) < 0)
-    //        cout << "recv function failed" << endl;
-    //    else
-    //        cout << "Data transfering successfully" << endl;
-    //    cout << receivedMessage << endl;
-    //    // Finally, we close socket
-    //    close(socket_desc);
+    establishBasicServer(2610);
     return 0;
 }
