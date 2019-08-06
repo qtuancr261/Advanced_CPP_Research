88366e0409cb4a90db60e65dffd0e2b01b721de8
qtuancr261 <tuancr261@gmail.com>
2019-08-06 23:34:05 +0700

    Init byte buffer wrapper class

---
+++ Socket/SocketServer/bufferwrapper.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: tuantq3
+ * File: bufferwrapper.cpp
+ *
+ * Created on 6/8/2019
+ */
+#include "bufferwrapper.h"
+
+BufferWrapper::BufferWrapper()
+{
+
+}
---
+++ Socket/SocketServer/bufferwrapper.h
@@ -0,0 +1,17 @@
+/*
+ * Author: tuantq3
+ * File: bufferwrapper.h
+ *
+ * Created on 6/8/2019
+ */
+#ifndef BUFFERWRAPPER_H
+#define BUFFERWRAPPER_H
+
+
+class BufferWrapper
+{
+public:
+    BufferWrapper();
+};
+
+#endif // BUFFERWRAPPER_H
