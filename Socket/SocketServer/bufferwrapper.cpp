aed27018001bc3dc5c75b673bb9aada08ccf2dbd
qtuancr261 <tuancr261@gmail.com>
2019-08-06 23:37:50 +0700

    Implemented write functions for int value (int8_t, int16_t, int32_t, int64_t) 	Init associate read functions for int value

--- Socket/SocketServer/bufferwrapper.cpp
+++ Socket/SocketServer/bufferwrapper.cpp
@@ -6,7 +6,46 @@
  */
 #include "bufferwrapper.h"
 
-BufferWrapper::BufferWrapper()
-{
+BufferWrapper::BufferWrapper(uint8_t *const srcData, size_t dataLen) : _data{srcData}, _sizeRemain{dataLen} {}
 
+BufferWrapper::~BufferWrapper() {}
+
+bool BufferWrapper::writeI8(int8_t value) {
+    if (_sizeRemain < sizeof(value)) return false;
+    *((int8_t *)_data) = value;
+    _data += sizeof(value);
+    _sizeRemain -= sizeof(value);
+    return true;
 }
+
+bool BufferWrapper::writeI16(int16_t value) {
+    if (_sizeRemain < sizeof(value)) return false;
+    *((int16_t *)_data) = value;
+    _data += sizeof(value);
+    _sizeRemain -= sizeof(value);
+    return true;
+}
+
+bool BufferWrapper::writeI32(int32_t value) {
+    if (_sizeRemain < sizeof(value)) return false;
+    (*(int32_t *)_data) = value;
+    _data += sizeof(value);
+    _sizeRemain -= sizeof(value);
+    return true;
+}
+
+bool BufferWrapper::writeI64(int64_t value) {
+    if (_sizeRemain < sizeof(value)) return false;
+    (*(int64_t *)_data) = value;
+    _data += sizeof(value);
+    _sizeRemain -= sizeof(value);
+    return true;
+}
+
+bool BufferWrapper::readI8(int8_t &value) {}
+
+bool BufferWrapper::readI16(int16_t &value) {}
+
+bool BufferWrapper::readI32(int32_t &value) {}
+
+bool BufferWrapper::readI64(int64_t &value) {}
--- Socket/SocketServer/bufferwrapper.h
+++ Socket/SocketServer/bufferwrapper.h
@@ -7,11 +7,30 @@
 #ifndef BUFFERWRAPPER_H
 #define BUFFERWRAPPER_H
 
+#include <bits/stdint-uintn.h>
+#include <stdint.h>
+#include <cstring>
+// This class doesn't have the ownership with the data it hold
+class BufferWrapper {
+private:
+    uint8_t* _data;
+    size_t _sizeRemain;
 
-class BufferWrapper
-{
 public:
-    BufferWrapper();
+    BufferWrapper(uint8_t* const srcData, size_t dataLen);
+    virtual ~BufferWrapper();
+
+public:  // write functions
+    bool writeI8(int8_t value);
+    bool writeI16(int16_t value);
+    bool writeI32(int32_t value);
+    bool writeI64(int64_t value);
+
+public:  // read functions
+    bool readI8(int8_t& value);
+    bool readI16(int16_t& value);
+    bool readI32(int32_t& value);
+    bool readI64(int64_t& value);
 };
 
-#endif // BUFFERWRAPPER_H
+#endif  // BUFFERWRAPPER_H
