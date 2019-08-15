49dce9cc47385855b8ae74ac59316b4fc8cdbf67
qtuancr261 <tuancr261@gmail.com>
2019-08-15 21:54:45 +0700

    Testing std::random_device as a seeds generator

--- RandomEngine/main.cpp
+++ RandomEngine/main.cpp
@@ -1,12 +1,29 @@
-#include <iostream>
-using std::move;
-using std::cout;
-int x = 10;
-int&& getRInt() {return move(x);}
-void printAddress(const int& v)
-{
+/*
+ * Author: tuantq3
+ * File: main.cpp
+ *
+ * Created on 15/8/2019
+ *
+ * Testing stl generating random numbers
+ */
 
-}
-int main(int argc, char *argv[])
-{
+#include <iostream>
+#include <random>
+using std::cout;
+using std::endl;
+using std::random_device;
+int main(int argc, char *argv[]) {
+    // std::random_device is a uniformly-distributed integer random number generator
+    // that produces non-deteministic random numbers
+    // Use std::random_device as a seeds generator
+    //
+    random_device randDev{};
+    cout << "Min rand value: " << randDev.min() << endl;
+    cout << "Max rand value: " << randDev.max() << endl;
+    // the max rand value is equal to std::numeric_limits<unsigned int>::max()
+    for (int iseed = 0; iseed < 10; ++iseed) {
+        cout << "Rand seed: " << randDev() << endl;
+    }
+    cout << "Entropy " << randDev.entropy() << endl;
+    return 0;
 }
