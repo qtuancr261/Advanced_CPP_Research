c6c1e4bedc89f82be81154d8e3a091a2f49d271f
qtuancr261 <tuancr261@gmail.com>
2019-08-18 10:26:49 +0700

    Updated test stl random 	- Wrap test for std::random_device 	- Init test for std::seed_seq (the same input value - different leng gen)

--- RandomEngine/main.cpp
+++ RandomEngine/main.cpp
@@ -8,11 +8,21 @@
  */
 
 #include <iostream>
+#include <iterator>
 #include <random>
+#include <vector>
+
+using std::copy;
 using std::cout;
 using std::endl;
 using std::random_device;
-int main(int argc, char *argv[]) {
+using std::seed_seq;
+using std::string;
+using std::vector;
+
+#define seperatorBeginEnd cout << endl << "-------------------------------------------------------------" << endl
+
+void testObtaningRandomSeeds(int numSeeds) {
     // std::random_device is a uniformly-distributed integer random number generator
     // that produces non-deteministic random numbers
     // Use std::random_device as a seeds generator
@@ -21,9 +31,45 @@ int main(int argc, char *argv[]) {
     cout << "Min rand value: " << randDev.min() << endl;
     cout << "Max rand value: " << randDev.max() << endl;
     // the max rand value is equal to std::numeric_limits<unsigned int>::max()
-    for (int iseed = 0; iseed < 10; ++iseed) {
+    // if the implementation is using a random number engine and not a non-deteministic source entropy value is equal to 0
+    for (int iseed = 0; iseed < numSeeds; ++iseed) {
         cout << "Rand seed: " << randDev() << endl;
     }
     cout << "Entropy " << randDev.entropy() << endl;
+    seperatorBeginEnd;
+}
+void genSeedSequences(seed_seq& initSeedSeq, const string& title, size_t numGenValues = 10) {
+    vector<uint> ranVec(numGenValues, 0);
+    initSeedSeq.generate(std::begin(ranVec), std::end(ranVec));
+
+    cout << title << endl;
+    copy(std::begin(ranVec), std::end(ranVec), std::ostream_iterator<uint>{cout, " - "});
+    seperatorBeginEnd;
+}
+int main(int argc, char* argv[]) {
+    // gen single seed value
+    testObtaningRandomSeeds(10);
+    // gen seed sequences
+    seed_seq seedSeq_1;  // default constructor
+    genSeedSequences(seedSeq_1, "seedSeq_1 - default constructor ");
+    genSeedSequences(seedSeq_1, "seedSeq_1 - default constructor AGAIN");
+    genSeedSequences(seedSeq_1, "seedSeq_1 - default constructor AGAIN (different gen num: 5)", 5);
+    seed_seq seedSeq_2{2610, 1996, 2018, 12};
+    genSeedSequences(seedSeq_2, "seedSeq_2 - consecutive int ");
+    genSeedSequences(seedSeq_2, "seedSeq_2 - consecutive int AGAIN");
+    genSeedSequences(seedSeq_2, "seedSeq_2 - consecutive int AGAIN (different gen num: 4)", 4);
+    genSeedSequences(seedSeq_2, "seedSeq_2 - consecutive int AGAIN");
+    genSeedSequences(seedSeq_2, "seedSeq_2 - consecutive int AGAIN (different gen num: 12)", 12);
+    random_device rd;
+    seed_seq seedSeq_3{rd(), rd()};
+    genSeedSequences(seedSeq_3, "seedSeq_3 - use random_device");
+    genSeedSequences(seedSeq_3, "seedSeq_3 - use random_device AGAIN (different gen num: 8)", 8);
+    genSeedSequences(seedSeq_3, "seedSeq_3 - use random_device AGAIN (different gen num: 16)", 16);
+    vector<int> initValue{2610, 1996, 2018, 12};
+    seed_seq seedSeq_4(std::begin(initValue), std::end(initValue));
+    genSeedSequences(seedSeq_4, "seedSeq_5 - use a range (the same as seedSeq_2)");
+    initValue.push_back(2000);
+    seed_seq seedSeq_5(std::begin(initValue), std::end(initValue));
+    genSeedSequences(seedSeq_5, "seedSeq_5 - use a range ");
     return 0;
 }
