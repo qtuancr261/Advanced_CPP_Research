6863512298bbf99f82465be93319223a62933885
qtuancr261 <tuancr261@gmail.com>
2019-08-21 11:46:56 +0700

    Wrapp test std::seed_seq and more explain comments

--- RandomEngine/main.cpp
+++ RandomEngine/main.cpp
@@ -40,16 +40,21 @@ void testObtaningRandomSeeds(int numSeeds) {
 }
 void genSeedSequences(seed_seq& initSeedSeq, const string& title, size_t numGenValues = 10) {
     vector<uint> ranVec(numGenValues, 0);
+    // You will get  a wide range of 32-bit integers generated
+    // Regardless of how you create the seed_seq object
+    // the generate() member produces as many different values as are necessary to fill the range you specify
     initSeedSeq.generate(std::begin(ranVec), std::end(ranVec));
+    // You can call generate() as many times as you like
+    // The values in a sequence produced by generate() depends on the length of the sequence
+    // Sequences of a given length will be the same per generate() call
+    // Sequences of different lengths will contain different values.
 
     cout << title << endl;
     copy(std::begin(ranVec), std::end(ranVec), std::ostream_iterator<uint>{cout, " - "});
     seperatorBeginEnd;
 }
-int main(int argc, char* argv[]) {
-    // gen single seed value
-    testObtaningRandomSeeds(10);
-    // gen seed sequences
+
+void testSeedSeq(size_t numGenValue = 10) {
     seed_seq seedSeq_1;  // default constructor
     genSeedSequences(seedSeq_1, "seedSeq_1 - default constructor ");
     genSeedSequences(seedSeq_1, "seedSeq_1 - default constructor AGAIN");
@@ -62,14 +67,24 @@ int main(int argc, char* argv[]) {
     genSeedSequences(seedSeq_2, "seedSeq_2 - consecutive int AGAIN (different gen num: 12)", 12);
     random_device rd;
     seed_seq seedSeq_3{rd(), rd()};
+    // if we use argument value create by std::random_device for seed_seq constructor
+    // the sequences will be different per program excution
     genSeedSequences(seedSeq_3, "seedSeq_3 - use random_device");
     genSeedSequences(seedSeq_3, "seedSeq_3 - use random_device AGAIN (different gen num: 8)", 8);
     genSeedSequences(seedSeq_3, "seedSeq_3 - use random_device AGAIN (different gen num: 16)", 16);
     vector<int> initValue{2610, 1996, 2018, 12};
+    // the same argument values to the seed_seq constructor the same sequences you will get when call generate() per program execution (same length as well)
     seed_seq seedSeq_4(std::begin(initValue), std::end(initValue));
     genSeedSequences(seedSeq_4, "seedSeq_5 - use a range (the same as seedSeq_2)");
     initValue.push_back(2000);
     seed_seq seedSeq_5(std::begin(initValue), std::end(initValue));
     genSeedSequences(seedSeq_5, "seedSeq_5 - use a range ");
+}
+
+int main(int argc, char* argv[]) {
+    // gen single seed value
+    testObtaningRandomSeeds(10);
+    // gen seed sequences
+    testSeedSeq();
     return 0;
 }
