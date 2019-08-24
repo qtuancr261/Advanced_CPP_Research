8467cadd505aacf9223de1ba2400cb5a37e3dcf2
qtuancr261 <tuancr261@gmail.com>
2019-08-24 17:20:44 +0700

    Updated std::thread examples

--- MultiThread_Examples/ConcurrentWorld/main.cpp
+++ MultiThread_Examples/ConcurrentWorld/main.cpp
@@ -14,22 +14,52 @@ struct funct {
             cout << var << " -> " << _refI << endl;
         }
     }
+
+    bool doTheSameThingAsFunctor() {
+        for (int var = 0; var < 5; ++var) {
+            cout << var << " -> " << 5 << endl;
+        }
+        return true;
+    }
 };
-int say() {
-    cout << "Hello concurrent world " << endl;
+int say(string& message) {
+    cout << message << endl;
     cout << " ------" << endl;
+    message.append(" - said");
+    cout << "Internal thread we say after appending: " << message << endl;
     return 1;
 }
+void sayHi() { cout << "Hi " << endl; }
 int main() {
-    thread thread_1{say};
-    thread thread_2{Worker(1)};
+    int i = 20;
+    funct F1{i};
+    // std::thread works with any callable type
+    // pass a function
+    thread thread_1{sayHi};
     thread_1.join();
+
+    // so we could pass an instance of a class with a function call operator to the std::thread
+    thread thread_2{F1};
     thread_2.join();
 
-    int i = 9000;
-    funct F1(i);
-    thread thread_3{F1};
-    // instead of use thread_3.join();
-    ThreadGuard guard1{thread_3};
+    // Or use the functor directly
+    thread thread_3{funct(i)};
+    thread_3.join();
+
+    //
+    thread thread_4{&funct::doTheSameThingAsFunctor, &F1};
+    thread_4.join();
+
+    string message{"Hello"};
+    // By default the arguments are copied into internal storage
+    // Where they can be access by the newly created thread of execution
+    // Even if the corresponding parameter in the function is expecting a ref
+    // So that you need to wrap the arguments that really need to be ref in std::ref()
+    thread thread_5{say, std::ref(message)};
+    thread_5.join();
+    cout << "outside the thread we say after appending: " << message << endl;
+
+    thread thread_6{Worker(1)};
+    thread_6.join();
     return 0;
 }
