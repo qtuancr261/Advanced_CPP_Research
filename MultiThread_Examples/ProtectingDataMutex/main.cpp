934bb1ba002ee0c258dc2a25c482b274c1326b2d
qtuancr261 <tuancr261@gmail.com>
2019-09-08 16:27:40 +0700

    Prepare new project (Chapter 3) using cmake instead of qmake

---
+++ MultiThread_Examples/ProtectingDataMutex/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: tuantq3
+ * File: %{Cpp:License:FileName}
+ *
+ * Created on 8/9/2019
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
