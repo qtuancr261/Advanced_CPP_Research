a821eb28ce67071a0509bbd19a4aef2c33f52f92
qtuancr261 <tuancr261@gmail.com>
2019-09-17 22:58:45 +0700

    simple example for std::lock_guard

--- MultiThread_Examples/ProtectingDataMutex/main.cpp
+++ MultiThread_Examples/ProtectingDataMutex/main.cpp
@@ -5,11 +5,34 @@
  * Created on 8/9/2019
  */
 #include <iostream>
+#include <vector>
+#include <mutex>
+#include <algorithm>
+
+using std::vector;
+using std::lock_guard;
+using std::mutex;
 
 using namespace std;
+class GuardVec {
+private:
+    vector<int> _data;
+    mutex _dataMutex;
+public:
+    GuardVec() = default;
+    ~GuardVec() = default;
+    void addNewValue(int newValue) {
+        lock_guard<mutex> wLock{_dataMutex};
+        _data.push_back(newValue);
+    }
 
-int main()
-{
+    bool isValueValid(int lookupValue) {
+        lock_guard<mutex> rLock{_dataMutex};
+	return (std::find(std::begin(_data), std::end(_data), lookupValue) != std::end(_data));
+    }
+};
+
+int main() {
     cout << "Hello World!" << endl;
     return 0;
 }
