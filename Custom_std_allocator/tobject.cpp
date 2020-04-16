4ee7e511a07c2725e1c95aa6ce058c77e6608151
tuantq3 <tuantq3@vng.com.vn>
2020-04-16 10:27:32 +0700

    Reformat

--- Custom_std_allocator/tobject.cpp
+++
@@ -1,7 +0,0 @@
-#include "tobject.h"
-
-TObject::TObject()
-    : id{0}, ip{"127.0.0.1"}
-{
-    // default contructor
-}
--- Socket/GetAddressPerNetworkIf/main.cpp
+++ Socket/GetAddressPerNetworkIf/main.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <ifaddrs.h>
+#include <netdb.h>
+#include <sys/socket.h>
 #include <cstring>
+#include <iostream>
+#include <string>
 using namespace std;
 void showIPPerNetworkIf() {
-    ifaddrs* ifaddr{nullptr};
-    ifaddrs* ifa{nullptr};
+    ifaddrs *ifaddr{nullptr};
+    ifaddrs *ifa{nullptr};
     int family{}, s{};
-    char host[NI_MAXHOST] {};
+    char host[NI_MAXHOST]{};
 
     if (getifaddrs(&ifaddr) == -1) {
         std::cerr << "get network interface failed ";
@@ -17,23 +18,23 @@ void showIPPerNetworkIf() {
     }
 
     for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
-        if (ifa->ifa_addr == nullptr)
-            continue;
+        if (ifa->ifa_addr == nullptr) continue;
         s = getnameinfo(ifa->ifa_addr, sizeof(sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-        if( /*(strcmp(ifa->ifa_name,"wlan0")==0)&&( */ ifa->ifa_addr->sa_family==AF_INET) // )
-                {
-                    if (s != 0)
-                    {
-                        printf("getnameinfo() failed: %s\n", gai_strerror(s));
-                        exit(EXIT_FAILURE);
-                    }
-                    printf("\tInterface : <%s>\n",ifa->ifa_name );
-                    printf("\t  Address : <%s>\n", host);
-                }
+        if (/*(strcmp(ifa->ifa_name,"wlan0")==0)&&( */ ifa->ifa_addr->sa_family == AF_INET)  // )
+        {
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
             }
-
-            freeifaddrs(ifaddr);
+            printf("\tInterface : <%s>\n", ifa->ifa_name);
+            printf("\t  Address : <%s>\n", host);
+            string hostAddress{host};
+            if (hostAddress == "127.0.0.1") cout << "local address" << endl;
+        }
     }
+
+    freeifaddrs(ifaddr);
+}
 int main() {
     showIPPerNetworkIf();
     return 0;
