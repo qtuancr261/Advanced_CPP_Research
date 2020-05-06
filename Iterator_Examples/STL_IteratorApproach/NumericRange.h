0236d3e1ad63741123029b867559d7141013c0b4
qtuancr261 <tuancr261@gmail.com>
2020-05-06 17:18:42 +0700

    Init NumericIterator template class

--- Iterator_Examples/STL_IteratorApproach/NumericRange.h
+++ Iterator_Examples/STL_IteratorApproach/NumericRange.h
@@ -8,6 +8,7 @@
 #define NUMERICRANGE_H
 
 #include <cstdlib>
+#include <iterator>
 #include <type_traits>
 //
 template <typename T>
@@ -30,6 +31,7 @@ public:
     explicit NumericRange(T startValue = 0, T stepValue = 1, size_t n = 2) : _start{startValue}, _step{stepValue}, _count{n} {
         // nothing here
     }
+    NumericRange(const NumericRange& srcRange) = default;
     // the begin iterator for the range
     NumIterator begin() { return NumIterator{*this}; }
     // the end iterator for the range
@@ -38,5 +40,33 @@ public:
         endIter.value = _start + _count * _step;
         return endIter;
     }
+};
+
+// NumericIterator template: forward iterator
+/* std::iterator template
+ * template<class Category, class T, class Difference = ptrdiff_t, class Pointer = T*, class Reference = T&>
+ * struct iterator {
+ *  typedef T value_type;
+ *  typedef Difference difference_type;
+ *  typedef Reference reference;
+ *  typedef Pointer pointer;
+ *  typedef Category iterator_category;
+ * };
+ * => class MyIter : public std::iterator<std::random_access_iterator_tag,int> {
+ *   // other members
+ * }
+ */
+template <typename T>
+class NumericIterator : public std::iterator<std::forward_iterator_tag, T> {
+    friend class NumericRange<T>;
+
+private:
+    // Ref to the range for this iterator
+    NumericRange<T>& _range;
+    // Value pointed to
+    T _value;
+
+public:
+    explicit NumericIterator(NumericRange<T>& srcRange) : _range{srcRange}, _value{srcRange.begin()} {}
 };
 #endif  // NUMERICRANGE_H
