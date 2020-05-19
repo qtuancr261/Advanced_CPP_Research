82a1c888c60522a40b3b4572c2d7f0d08dc92dae
qtuancr261 <tuancr261@gmail.com>
2020-05-19 17:38:37 +0700

    Init Chapter4 example

---
+++ MultiThread_Examples/SyncConcurrentOps/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Author: tuantq3
+ * File: %{Cpp:License:FileName}
+ *
+ * Created on 19/5/2020
+ */
+#include <condition_variable>
+#include <iostream>s
+#include <mutex>
+
+int main() {
+    std::cout << "Hello World!" << endl;
+    return 0;
+}
