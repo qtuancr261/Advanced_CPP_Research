92f695fe0b65d7409c8ea4b6d8b02a16393aa1b9
qtuancr261 <tuancr261@gmail.com>
2020-05-22 08:12:33 +0700

    Update prepareJob

--- MultiThread_Examples/SyncConcurrentOps/main.cpp
+++ MultiThread_Examples/SyncConcurrentOps/main.cpp
@@ -14,24 +14,24 @@
 #include <thread>
 std::random_device randSeed;
 std::mutex mutexQueue;
-std::uniform_int_distribution<uint32_t> randDis;
-std::default_random_engine randEngine;
+
 std::queue<uint32_t> jobsQueue;
 std::condition_variable jobCondition;
-std::once_flag flagInit;
-void initJobGen(int seed) {
+void initJobGen(std::default_random_engine& randEng, int seed) {
     std::cout << "Thread seed: " << seed << std::endl;
-    randEngine.seed(seed);
+    randEng.seed(seed);
 }
 void prepareJobs(int seed) {
+    static thread_local std::default_random_engine randEng;
+    static thread_local std::once_flag flagInit;
     try {
-        std::call_once(flagInit, initJobGen, seed);
+        std::call_once(flagInit, initJobGen, randEng, seed);
     } catch (std::exception& exp) {
         std::cout << exp.what();
     }
-
+    static thread_local std::uniform_int_distribution<uint32_t> randDis;
     while (true) {
-        uint32_t newJobs{randDis(randEngine)};
+        uint32_t newJobs{randDis(randEng)};
         std::cout << "JobID: " << newJobs << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
@@ -43,7 +43,7 @@ int main() {
     std::cout << "Today seed is : " << toDaySeed << std::endl;
     std::seed_seq seedSeq{toDaySeed};
     seedSeq.generate(std::begin(threadSeeds), std::end(threadSeeds));
-    std::copy(std::begin(threadSeeds), std::end(threadSeeds), std::ostream_iterator<uint32_t>(std::cout, " - "));
-    // prepareJobs();
+    std::copy(std::begin(threadSeeds), std::end(threadSeeds), std::ostream_iterator<uint32_t>(std::cout, " | "));
+    prepareJobs(threadSeeds[0]);
     return 0;
 }
