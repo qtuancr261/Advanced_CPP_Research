4459b84765be32b77946c58e0c6ed297f1eb7e42
qtuancr261 <tuancr261@gmail.com>
2020-07-22 17:10:12 +0700

    Doing some benchmark for FeedBackRegister

--- Bitwise_op/StreamCipher/main.cpp
+++ Bitwise_op/StreamCipher/main.cpp
@@ -8,12 +8,29 @@
 #include <string>
 #include "FeedBackRegister.h"
 #include "google_benchmark/inc/benchmark.h"
+#include <random>
 using namespace std;
 static void BM_StringCreation(benchmark::State& state) {
     for (auto _ : state) std::string empty_string;
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
+
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+static void BM_FeedbackRegister(benchmark::State& state) {
+    std::default_random_engine _randEngine;
+    std::uniform_int_distribution<uint64_t> dis;
+    for (auto _ : state) {
+        FeedBackRegister<uint64_t> _uintFSB{dis(_randEngine)};
+    }
+}
+BENCHMARK(BM_FeedbackRegister);
 BENCHMARK_MAIN();
 // int main() {
 //    FeedBackRegister<uint64_t> _fint{92388325570ull};
