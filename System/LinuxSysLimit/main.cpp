7fd82460f3bf1e786038270d7cd5fe2f1008ca91
qtuancr261 <tuancr261@gmail.com>
2020-11-09 10:52:59 +0700

    PAGE SIZE

--- System/LinuxSysLimit/main.cpp
+++ System/LinuxSysLimit/main.cpp
@@ -4,12 +4,13 @@
  *
  * Created on 19/8/2020
  */
+#include <sys/mman.h>
+#include <unistd.h>
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main() {
+	long pageSize{sysconf(_SC_PAGESIZE)};
+	std::cout << "Sys page size in byte: " << pageSize << std::endl;
+	return 0;
 }
